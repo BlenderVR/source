4f6141e02ab56cdcc17a35ab68d4a4b058f61815
Rye Terrell <wwwtyro@gmail.com>
2014-04-24 22:54:06 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup ( \
+	name = 'python-ovrsdk',
+    version = '0.1',
+    description = 'Python wrapper for Oculus VR SDK.',
+    author = 'Rye Terrell',
+    author_email = 'wwwtyro@gmail.com',
+    url = 'https://github.com/wwwtyro/python-ovrsdk',
+    packages = ['ovrsdk']
+)
