ba56196e32b6bf130ec5388bf52add7acb19c458
rye terrell <ryterrell@ryeterrell.net>
2014-06-07 15:41:13 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, find_packages
 setup ( \
     name = 'python-ovrsdk',
-    version = '0.3.2.1',
+    version = '0.3.2.2',
     description = 'Python wrapper for Oculus VR SDK.',
     long_description = open('README.rst', 'r').read(),
     author = 'Rye Terrell',
     author_email = 'wwwtyro@gmail.com',
     url = 'https://github.com/wwwtyro/python-ovrsdk',
-    packages = ['ovrsdk', 'ovrsdk.windows'],
-    package_data = {'': ['libovr.dll']}
+    packages = ['ovrsdk', 'ovrsdk.windows', 'ovrsdk.linux'],
+    package_data = {'': ['libovr.dll', 'libovr.so']}
 )
