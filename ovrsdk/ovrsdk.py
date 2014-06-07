dd263c8f007551f731e9f90614989460fd607603
rye terrell <ryterrell@ryeterrell.net>
2014-06-07 10:09:50 -0500

    typofix

--- ovrsdk/ovrsdk.py
+++ ovrsdk/ovrsdk.py
@@ -5,7 +5,7 @@ import sys
 
 if "win" in sys.platform:
 	from windows import *
-else if "linux" in sys.platform:
+elif "linux" in sys.platform:
     from linux import *
 else:
 	raise Exception("The %s platform is not supported. Aborting." % sys.platform)
