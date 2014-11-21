b77770c0f2b51ee3a04ddb4b4abecd86709e5d2e
Dalai Felinto <dfelinto@gmail.com>
2014-11-21 19:09:24 -0200

    Doc: expand dummy modules to the remaining required QT classes

--- docs/dummy/PySide.py
+++ docs/dummy/PySide.py
@@ -6,6 +6,19 @@ class QtGui:
         def __init__(self):
             pass
 
+    class QtGui:
+        def __init__(self):
+            pass
+
+    class QMainWindow:
+        def __init__(self):
+            pass
+
+    class QApplication:
+        def __init__(self, foo):
+            pass
+
+
 class QtCore:
     def __init__(self):
         pass
