a63d150861e437c0aa4684eee02795d95a625cac
Dalai Felinto <dfelinto@gmail.com>
2014-11-21 19:32:26 -0200

    Doc: change build dir and stop showing the code in the documentation
    The build dir changed from _build to build

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../modules'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
+#    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -71,7 +71,7 @@ release = '1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['build', 'dummy']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
--- docs/update-docs.sh
+++ docs/update-docs.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# generate the rst files
 sphinx-apidoc -o modules/rst/ --force --separate --maxdepth=1 ../modules/blendervr/
 sphinx-apidoc -o utils/rst --force --no-headings --separate --maxdepth=1 ../utils/
+
+# remote the unused files
+rm modules/rst/modules.rst
+rm utils/rst/modules.rst
+
+# compile the docs
 make html
