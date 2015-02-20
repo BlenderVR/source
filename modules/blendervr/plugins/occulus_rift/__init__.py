9c59eb0de1ba3d3c1df52d7cd80b037f3b64707e
Dalai Felinto <dfelinto@gmail.com>
2015-02-20 03:43:54 -0200

    Fixup for Occulus Rift plugin

--- modules/blendervr/plugins/occulus_rift/__init__.py
+++ modules/blendervr/plugins/occulus_rift/__init__.py
@@ -49,7 +49,7 @@ if base.is_virtual_environment():
             self._main = virtual_environment.OcculusRift(self, configuration)
 
         def start(self):
-            if not hasattr(self, '_main'):
+            if not hasattr(self, '_main') or not self._main.isAvailable():
                 raise exceptions.PluginError()
             self.run = self._main.run
             self._main.start()
