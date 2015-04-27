19c2a4b244701bc81a96eeccec5be0baa458e61a
Dalai Felinto <dfelinto@gmail.com>
2015-04-27 19:49:52 -0300

    Oculus DK2: store glsl script externally

--- modules/blendervr/loader/__init__.py
+++ modules/blendervr/loader/__init__.py
@@ -99,7 +99,7 @@ class Creator:
                 TEXT = ELEMENTS_MAIN_PREFIX + 'Oculus:GLSL'
                 shader = bpy.data.texts.new(TEXT)
                 actuator.glsl_shader = shader
-                shader.from_string(SHADER_PROGRAM)
+                shader.from_string(self._getScreenShader())
 
                 bpy.ops.object.game_property_new(type='FLOAT', name="screen_width")
                 bpy.ops.object.game_property_new(type='FLOAT', name="screen_height")
@@ -123,65 +123,15 @@ class Creator:
         elif is_console():
             print(self._output_blender_file)
 
+    def _getScreenShader(self):
+        import os
+        folderpath = os.path.dirname(os.path.abspath(__file__))
+        filepath = os.path.join(folderpath, 'oculus_dk2.glsl')
+        f = open(filepath, 'r')
+        data = f.read()
+        f.close()
+        return data
 
-SHADER_PROGRAM = """
-uniform sampler2D bgl_RenderedTexture;
-
-const vec4 kappa = vec4(1.0,1.7,0.7,15.0);
-
-uniform float screen_width;
-uniform float screen_height;
-
-const float scaleFactor = 0.6;
-
-const vec2 leftCenter = vec2(0.25, 0.5);
-const vec2 rightCenter = vec2(0.75, 0.5);
-
-// Scales input texture coordinates for distortion.
-vec2 hmdWarp(vec2 LensCenter, vec2 texCoord, vec2 Scale, vec2 ScaleIn) {
-    vec2 theta = (texCoord - LensCenter) * ScaleIn;
-    float rSq = theta.x * theta.x + theta.y * theta.y;
-    vec2 rvector = theta * (kappa.x + kappa.y * rSq + kappa.z * rSq * rSq + kappa.w * rSq * rSq * rSq);
-    vec2 tc = LensCenter + Scale * rvector;
-    return tc;
-}
-
-bool validate(vec2 tc, int left_eye) {
-    //keep within bounds of texture
-    if ((left_eye == 1 && (tc.x < 0.0 || tc.x > 0.5)) ||
-        (left_eye == 0 && (tc.x < 0.5 || tc.x > 1.0)) ||
-        tc.y < 0.0 || tc.y > 1.0) {
-        return false;
-    }
-    return true;
-}
-
-void main() {
-    vec2 screen = vec2(screen_width, screen_height);
-
-    float as = float(screen.x / 2.0) / float(screen.y);
-    vec2 Scale = vec2(0.5, as);
-    vec2 ScaleIn = vec2(2.0 * scaleFactor, 1.0 / as * scaleFactor);
-
-    vec2 texCoord = gl_TexCoord[0].st;
-
-    vec2 tc = vec2(0);
-    vec4 color = vec4(0);
-
-    if (texCoord.x < 0.5) {
-        tc = hmdWarp(leftCenter, texCoord, Scale, ScaleIn );
-        color = texture2D(bgl_RenderedTexture, tc);
-        if (!validate(tc, 1))
-            color = vec4(0);
-    } else {
-        tc = hmdWarp(rightCenter, texCoord, Scale, ScaleIn);
-        color = texture2D(bgl_RenderedTexture, tc);
-        if (!validate(tc, 0))
-            color = vec4(0);
-    }
-    gl_FragColor = color;
-}
-"""
 
 import os
 if os.environ.get('READTHEDOCS', False) != 'True':
